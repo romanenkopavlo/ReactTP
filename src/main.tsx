2282e3c3953a4865ed6fe9511b3d5f37514e6cbd
promanenko <paihitetty444@gmail.com>
2025-01-06 11:27:52 +0100

    Le debut

--- src/main.tsx
+++ src/main.tsx
@@ -18,6 +18,7 @@ const router = createBrowserRouter([
     }
 ])
 
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <RouterProvider router={router}/>
